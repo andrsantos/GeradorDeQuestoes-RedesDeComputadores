491271d2f73765b095a337b11d09385cbcc19b3f
AndreySantos <santosandrey146@gmail.com>
2026-06-03 22:27:21 -0300

    Corrigindo URLS - 03/06/2026

--- gerador-de-questoes-ui/src/app/services/prompts/prompt-service.ts
+++ gerador-de-questoes-ui/src/app/services/prompts/prompt-service.ts
@@ -8,7 +8,7 @@ import { Prompt } from '../../models/prompt.model';
 })
 export class PromptService {
   
-  private apiUrl = 'http://localhost:8080/api/prompts';
+  private apiUrl = 'http://187.77.240.149:83/api/prompts';
 
   constructor(private http: HttpClient) {}
 
